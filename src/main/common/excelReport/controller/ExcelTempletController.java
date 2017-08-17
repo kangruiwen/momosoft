429c49c782ca1aa3f3895b81e36fcfcfe04922cc
momo <271858344@qq.com>
2017-08-17 08:46:17 +0800

    commit

--- src/main/common/excelReport/controller/ExcelTempletController.java
+++ src/main/common/excelReport/controller/ExcelTempletController.java
@@ -1,8 +1,21 @@
 package main.common.excelReport.controller;
+
+import java.util.List;
+
+import main.common.excelReport.model.ExcelColumn;
+
 /**
  * @author momo
  * @time 2017年8月15日下午4:26:33
  */
 public class ExcelTempletController {
-
+	
+	
+	
+	/*
+	 * Excel的插入
+	 */
+	public void insertExcelExample(List<ExcelColumn> list) {
+		
+	}
 }
